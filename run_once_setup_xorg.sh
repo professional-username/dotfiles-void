73494b15f34300b56554900440986929b7ce3812
username <9hacahn5@anonaddy.me>
2022-02-26 16:39:05 +0000

    Added keyboard layout configuration

--- run_once_setup_xorg.sh
+++ run_once_setup_xorg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Copy synaptincs configs to /etc/X11/xorg.conf.d/
+# Copy xorg configs to /etc/X11/xorg.conf.d/
 sudo mkdir /etc/X11/xorg.conf.d/
 sudo ln $HOME/.config/xorg/70-synaptics.conf \
 	/etc/X11/xorg.conf.d/70-synaptics.conf
+sudo ln $HOME/.config/xorg/00-keyboard.conf \
+	/etc/X11/xorg.conf.d/00-keyboard.conf
