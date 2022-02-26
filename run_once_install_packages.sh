535ee8d89d786655eb5ad75203977feb3fbe27cf
username <9hacahn5@anonaddy.me>
2022-02-26 15:10:13 +0000

    Added vim-plug install

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -11,4 +11,9 @@ sudo xbps-install 	\
 	polybar 	\
 	jetbrains-jdk-bin \
 	feh 		\
-	xtools		\
+	xtools		
+
+
+# Install some packages outside of xbps
+# vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
