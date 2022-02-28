0eb01009d314cdc75373581134e0a975c91421c8
username <9hacahn5@anonaddy.me>
2022-02-28 20:01:30 +0000

    Fixed picom fork installation

--- run_once_install_picom_ibhagwan.sh
+++ run_once_install_picom_ibhagwan.sh
@@ -3,5 +3,5 @@
 # Install the ibhagwan picom fork
 git clone https://github.com/ibhagwan/picom-ibhagwan-template
 mv picom-ibhagwan-template $HOME/void-packages/srcpkgs/picom-ibhagwan
-./$HOME/void-packages/xbps-src pkg picom-ibhagwan
+$HOME/void-packages/xbps-src pkg picom-ibhagwan
 xi -f picom-ibhagwan
